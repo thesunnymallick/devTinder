a7726aea749812cfda5ef9e1d44854144874e7f1
thesunnymallick <alfesunnymallick800@gmail.com>
2026-07-06 10:42:01 +0530

    user update successfully

--- src/app.js
+++ src/app.js
@@ -1,30 +1,72 @@
 const express = require("express");
 const connectDB = require("./config/db");
-const User=require("./model/user")
+const User = require("./model/user");
 const app = express();
 
-  app.get("/", (req, res)=>{
-    res.send("hello")
-  })
+app.use(express.json());
 
-app.post("/singup", async(req, res)=>{
-   try {
-    const user={
-        firstName:"Sunny",
-        lastName:"Mallick",
-        password:"Sunny@123",
-        age:24,
-    }
-   
-    const newUser=new User(user);
+app.post("/singup", async (req, res) => {
+  try {
+    const newUser = new User(req.body);
     await newUser.save();
+    res.status(200).send("User create sucessfully");
+  } catch (error) {
+    res.status(500).send("Something went wrong");
+  }
+});
 
-    res.status(200).send("User create sucessfully")
+app.get("/profile/:id", async (req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.params.id });
+    res.status(200).json({
+      data: {
+        user,
+        message: "user featch successfully",
+      },
+    });
+  } catch (error) {
+    res.status(500).send("Something went wrong");
+  }
+});
 
-   } catch (error) {
-     res.status(500).send("Something went wrong")
-   }
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json({
+      data: {
+        users,
+        message: "fetch all users",
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("something went wrong");
+  }
+});
+
+app.post("/deleteUser", async (req, res) => {
+  try {
+    const users=await findOneAndDelete({_id: req.body.id})
+    res.status(200).json("User delete successfully")
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.patch("/updateUser", async(req, res)=>{
+    try {
+      const user=await findByIdAndUpdate({_id: req.body.id}, req.body);
+       res.status(201).json({
+        data:{
+          user
+        }, 
+        message:"user update successfully"
+       })
+    } catch (error) {
+        console.log(error)
+    }
 })
+
 
 connectDB()
   .then(() => {
