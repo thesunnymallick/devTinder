aee6d9474b7a61eab34d731833aa68991bf8ac83
thesunnymallick <alfesunnymallick800@gmail.com>
2026-07-12 22:49:25 +0530

    pagination added

--- src/controllers/feedController.js
+++ src/controllers/feedController.js
@@ -3,24 +3,27 @@ const User = require("../model/user");
 
 const feedController = async (req, res) => {
   try {
-    const loginUserId = req.user.id;
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 10;
+    const skip = (page-1)*limit;
 
+    const loginUserId = req.user.id;
     const allReadyConnection = await Connection.find({
       $or: [{ fromUserId: loginUserId }, { toUserId: loginUserId }],
     }).select(["fromUserId", "toUserId"]);
-
     const hideUsers = new Set();
     hideUsers.add(loginUserId.toString());
     allReadyConnection.forEach((item) => {
       hideUsers.add(item.fromUserId.toString());
       hideUsers.add(item.toUserId.toString());
     });
-
-
     const users = await User.find({
-        _id: {
-          $nin: [...hideUsers],
-        },
+      _id: {
+        $nin: [...hideUsers],
+      },
+    }).skip(skip).limit(limit);
+    const totalUsers = await User.countDocuments({
+        _id: { $nin: [...hideUsers] },
       });
 
     res.status(200).json({
@@ -29,6 +32,14 @@ const feedController = async (req, res) => {
         success: true,
         message: "all data fetch sucessfully",
         users,
+        pagination: {
+            page,
+            limit,
+            totalUsers,
+            totalPages: Math.ceil(totalUsers / limit),
+            hasNextPage: page < Math.ceil(totalUsers / limit),
+            hasPreviousPage: page > 1,
+          },
       },
     });
   } catch (error) {
